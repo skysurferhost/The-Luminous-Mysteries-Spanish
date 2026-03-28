917f01c0fd08eddce2b8ed377c55306966b78d89
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-03-28 10:18:53 -0400

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan",
+      "name": "1. El Bautismo en el Jordán",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6649957818216627,
+          "pitch": 0.08677225450899329,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6319923911907424,
+          "pitch": -0.17290606185455815,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">El Bautismo en el Jordán</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Entonces Jesús fue desde Galilea hasta el Jordán y se presentó a Juan para ser bautizado por él. Juan se resistía, diciéndole: «Soy yo el que tiene necesidad de ser bautizado por ti, ¡y eres tú el que viene a mi encuentro!». Pero Jesús le respondió: «Ahora déjame hacer esto, porque conviene que así cumplamos todo lo que es justo». Y Juan se lo permitió.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Apenas fue bautizado, Jesús salió del agua. En ese momento se abrieron los cielos, y vio al Espíritu de Dios descender como una paloma y dirigirse hacia él. Y se oyó una voz del cielo que decía: «Este es mi Hijo muy querido, en quien tengo puesta toda mi predilección».<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Mateo 3:13–17)<o:p></o:p></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-d-tic-ci-cana",
+      "name": "2. Las bodas de Caná",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20790684707299256,
+          "pitch": -0.008690813679965714,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        },
+        {
+          "yaw": 1.3359429840287653,
+          "pitch": 0.13937177954095148,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24373764674771792,
+          "pitch": -0.2789593316817669,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Las bodas de Caná</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Tres días después se celebraron unas bodas en Caná de Galilea, y la madre de Jesús estaba allí. Jesús también fue invitado con sus discípulos. Y como faltaba vino, la madre de Jesús le dijo: «No tienen vino». Jesús le respondió: «Mujer, ¿qué tenemos que ver nosotros? Mi hora no ha llegado todavía». Pero su madre dijo a los sirvientes: «Hagan todo lo que él les diga».</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Había allí seis tinajas de piedra destinadas a los ritos de purificación de los judíos, que contenían unos cien litros cada una. Jesús dijo a los sirvientes: «Llenen de agua estas tinajas». Y las llenaron hasta el borde. «Saquen ahora, agregó Jesús, y lleven al encargado del banquete». Así lo hicieron. El encargado probó el agua cambiada en vino y como ignoraba su o rigen, aunque lo sabían los sirvientes que habían sacado el agua, llamó al esposo y les dijo: «Siempre se sirve primero el buen vino y cuando todos han bebido bien, se trae el de inferior calidad. Tú, en cambio, has guardado el buen vino hasta este momento». Este fue el primero de los signos de Jesús, y lo hizo en Caná de Galilea. Así manifestó su gloria, y sus discípulos creyeron en él.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Juan 2:1–11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi",
+      "name": "3. El anuncio del Reino de Dios",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05358848048561171,
+          "pitch": -0.17051613309329028,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        },
+        {
+          "yaw": -1.7029965550555701,
+          "pitch": 0.1012106739874703,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004657148241202336,
+          "pitch": -0.4721606572718109,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">El anuncio del Reino de Dios</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Después que Juan fue arrestado, Jesús se dirigió a Galilea. Allí proclamaba la Buena Noticia de Dios, diciendo: 15 «El tiempo se ha cumplido: el Reino de Dios está cerca. Conviértanse y crean en la Buena Noticia».<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Marcos 1:14–15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-bin-hnh-trn-ni",
+      "name": "4. La Transfiguración",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7953274170598537,
+          "pitch": 0.37459110494686243,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-lp-b-tch-thnh-th"
+        },
+        {
+          "yaw": -1.748073222986461,
+          "pitch": -0.012033693567310877,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.71832037522912,
+          "pitch": -0.1197861019002886,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">La Transfiguración</span><o:p></o:p></p>",
+          "text":  "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Seis días después, Jesús tomó a Pedro, a Santiago y a su hermano Juan, y los llevó aparte a un monte elevado. Allí se transfiguró en presencia de ellos: su rostro resplandecía como el sol y sus vestiduras se volvieron blancas como la luz. De pronto se les aparecieron Moisés y Elías, hablando con Jesús. Pedro dijo a Jesús: «Señor, ¡qué bien estamos aquí! Si quieres, levantará aquí mismo tres carpas, una para ti, otra para Moisés y otra para Elías». Todavía estaba hablando, cuando una nube luminosa los cubrió con su sombra y se oyó una voz que decía desde la nube: «Este es mi Hijo muy querido, en quien tengo puesta mi predilección: escúchenlo». Al oír esto, los discípulos cayeron con el rostro en tierra, llenos de temor. Jesús se acercó a ellos, y tocándolos, les dijo: «Levántense, no tengan miedo». Cuando alzaron los ojos, no vieron a nadie más que a Jesús solo.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Mateo 17:1–8)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-lp-b-tch-thnh-th",
+      "name": "5. La Institución de la Eucaristía",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7996773456601955,
+          "pitch": 0.0118117757142997,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06865999545901147,
+          "pitch": -0.5026090379846924,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">La Institución de la Eucaristía</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Mientras comían, Jesús tomó el pan, pronunció la bendición, lo partió y lo dio a sus discípulos, diciendo: «Tomen y coman, esto es mi Cuerpo». Después tomó una copa, dio gracias y se la entregó, diciendo: «Beban todos de ella, porque esta es mi Sangre, la Sangre de la Alianza, que se derrama por muchos para la remisión de los pecados.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Mateo 26:26–28)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
